6c6cdf9a337d2f215500b666afb9326832f84ae8
Eduarda Amorim <eduarda.amorim@maximoveis.local>
2025-05-30 20:22:52 -0300

    Adiciona validação de chave de API no endpoint de imóveis

--- api_imoveis/auth.py
+++ api_imoveis/auth.py
@@ -3,7 +3,7 @@
 from fastapi import HTTPException, Security
 from fastapi.security.api_key import APIKeyHeader
 
-API_KEY = "bbgcosnb6545dcnhvf16546jcb"  # Substitua por sua chave real
+API_KEY = "teste"  # Substitua por sua chave real
 API_KEY_NAME = "chave"
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
